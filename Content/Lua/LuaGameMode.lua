9665e1a5d4a23cf2ccab0e5cc20762936484d1d4
木尘痕 <muchenhen@gmail.com>
2023-09-27 20:48:48 +0800

    modify submodule

--- Content/Lua/LuaGameMode.lua
+++
@@ -1,9 +0,0 @@
-local LuaGameMode = {}
-
-function LuaGameMode:ReceiveBeginPlay()
-    -- call super ReceiveBeginPlay
-    self.Super:ReceiveBeginPlay()
-    print("gamemode:ReceiveBeginPlay")
-end
-
-return Class(nil, nil, LuaGameMode)
